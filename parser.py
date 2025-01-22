bb5736e084e2f9fef23559aa31fdcd0656e9d19e
personnumber3377 <sontapaa.jokulainen@gmail.com>
2025-01-22 13:50:11 +0200

    Added some bullshit

--- parser.py
+++ parser.py
@@ -2,15 +2,12 @@
 TEST_FILE_NAME = "DrawString.emf"
 
 
-from header import * # For parsing the header etc...
+# from header import * # For parsing the header etc...
 import struct
 
 TEST = False
 
-def parse_header(data):
-	# EmfMetafileHeaderExtension2
-	h, restofdata = read_header(data) # Read the header from the data...
-	return h, restofdata
+
 
 '''
 
@@ -65,60 +62,9 @@ labeled "UndefinedSpace" are optional and MUST be ignored.
 import copy
 import struct
 
-def read_bytes(buffer, n): # Cuts first n bytes from buffer. Returns tuple where first element is the cut bytes and second element is the rest of the data.
-	return buffer[:n], buffer[n:]
+from emf_file import * # For parsing the stuff.
 
-def parse_records(record_data):
-	# Returns a list of record objects...
-	b = record_data # Keep track of the rest of the data
-	while b:
-		# Unpack type and length.
-		assert len(b) >= 8 # Should be atleast 8 bytes for the type and length.
-		t_and_l = b[:8]
-		# try to unpack the stuff here
-		t, l = struct.unpack('II', t_and_l) # Unpack two little endian integers.
-		rec_bytes = b[:l] # Cutoff at l.
-		print("Here are the record bytes: "+str(rec_bytes))
-		#print(t == 0x0000004C)
-		if TEST and t == 0x0000004C: # EMR_BITBLT record
-			print("EMR_BITBLT")
-			stuff = copy.deepcopy(rec_bytes)
-			_, stuff = read_bytes(stuff, 4) # Skip type
-			_, stuff = read_bytes(stuff, 4) # Skip length
-			bounds, stuff = read_bytes(stuff, 16) # Bounds which is a RectL object.
-			xDest, stuff = read_bytes(stuff, 4) # xDest
-			yDest, stuff = read_bytes(stuff, 4) # yDest
 
-			cxDest, stuff = read_bytes(stuff, 4) # cxDest
-			cyDest, stuff = read_bytes(stuff, 4) # cyDest
-
-			# BitBltRasterOperation
-			BitBltRasterOperation, stuff = read_bytes(stuff, 4) # cyDest
-
-			xSrc, stuff = read_bytes(stuff, 4) # xSrc
-			ySrc, stuff = read_bytes(stuff, 4) # ySrc
-
-			# XformSrc
-			xSrc, stuff = read_bytes(stuff, 24) #  An XForm object (section 2.2.28) that specifies a world-space to pagespace transform to apply to the source bitmap.
-
-			BkColorSrc, stuff = read_bytes(stuff, 4) # BkColorSrc
-			# UsageSrc
-			UsageSrc, stuff = read_bytes(stuff, 4) # UsageSrc
-			offBmiSrc, stuff = read_bytes(stuff, 4) # offBmiSrc
-
-			cbBmiSrc, stuff = read_bytes(stuff, 4) # cbBmiSrc
-
-			offBitsSrc, stuff = read_bytes(stuff, 4) # offBitsSrc
-			cbBitsSrc, stuff = read_bytes(stuff, 4) # cbBitsSrc
-			#cbBitsSrc, stuff = read_bytes(stuff, 4)
-			BitmapBuffer = stuff # This should be rest of the stuff.
-			print("BitmapBuffer: "+str(BitmapBuffer))
-			BitBltRasterOperation = int.from_bytes(BitBltRasterOperation)# struct.unpack("I", BitBltRasterOperation)
-			print("BitBltRasterOperation: "+str(hex(BitBltRasterOperation)))
-			print("Done!!!"*100)
-
-		b = b[l:] # Cutoff the thing
-	return
 
 def test_parser(): # Parse a known EMF file...
 	global TEST
@@ -127,10 +73,10 @@ def test_parser(): # Parse a known EMF file...
 	data = fh.read()
 	fh.close()
 	# Now parse header...
-	h, rest_of_data = parse_header(data)
+	# h, rest_of_data = parse_header(data)
 	# Now try to parse the records
-	records = parse_records(rest_of_data) # Try to parse the records from the data.
-
+	# records = parse_records(rest_of_data) # Try to parse the records from the data.
+	emf_obj = parse_emf_file(data)
 
 
 if __name__=="__main__":
